6665378373c753831498d4f59ddd299b6cb1226d
boyko1989 <hunwiss89@gmail.com>
2022-10-02 14:17:28 +0400

    Function execute whith right types
    Типы данных теперь можно указывать при заведении переменных,
    и как и в случае с модулем sys, чтобы использовать данные
    аргументы в скрипте, нужно обратиться к Namespace:
    
    - args.a
    - args.b

--- 03_argument_type.py
+++ 03_argument_type.py
@@ -3,11 +3,13 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Позиционные аргументы
-parser.add_argument("a")
-parser.add_argument("b")
+parser.add_argument("a", type=int)
+parser.add_argument("b", type=int)
 
 args = parser.parse_args()
 print(args)
 
 def summarize (a, b):
     print(a + b)
+
+summarize(args.a, args.b)
