b2b9fb4f029cfa9e59560fdb0a03b0f675d8976f
boyko1989 <hunwiss89@gmail.com>
2022-10-02 09:41:30 +0400

    First module
    Создал модуль и импортировал его в скрипт

---
+++ module.py
@@ -0,0 +1,14 @@
+GLOBAL = 'GLOBAL'
+
+def sum_module(a,b):
+    return a + b
+
+def mul_module(a,b):
+    return a * b
+
+def _im_hidden():
+    print('I\'m hidden!')
+
+# if __name__ == '__main__':
+#      print('Hi there, I am module!')
+#      print('Sum of 10 and 10: ', sum_module(10,10))
