635753cb5e8708d63a9890b99f2887099f427920
boyko1989 <hunwiss89@gmail.com>
2022-10-02 14:32:17 +0400

    Add optional argument
    Таково типа аргументы настраиваются гораздо плавнее.
    Можно настроить значение по умолчанию.

--- 03_argument_type.py
+++ 03_argument_type.py
@@ -6,10 +6,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("a", type=int, help="First argument")
 parser.add_argument("b", type=int, help="Second argument")
 
+# Опциональные аргументы
+
+parser.add_argument("-a", "--action", help="What to do with numbers", default="sumarize")
+
 args = parser.parse_args()
 print(args)
 
 def summarize (a, b):
     print(a + b)
 
-summarize(args.a, args.b)
+def minusize (a, b):
+    print(a - b)
+
+if args.action == 'sumarize':
+    summarize(args.a, args.b)
+elif args.action == 'minusuze':
+    minusize(args.a, args.b)
