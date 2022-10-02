6b13d124cb62cae3c9ef3a57ea39141a541f90a8
boyko1989 <hunwiss89@gmail.com>
2022-10-02 11:02:58 +0400

    Use function main() in construction if __name__
    Традиционное использование данной конструкции. В функции main()
    можно прописать абсолютно всё, к чему нужно обратиться при
    выполнении данного скрипта.
    
    Так можно писать полноценные программы: декомпозируем задачу,
    расписываем её по функциям, пишем логику в main() и нашей
    любимой конструкцией вызываем её.

--- module.py
+++ module.py
@@ -13,11 +13,13 @@ def _im_hidden():
     print('I\'m hidden!')
 
 
-def get_hidden():
+def main():
     _im_hidden()
+    print(sum_module(1, 3))
 
 
 if __name__ == '__main__':
-    print('Hi there, I am module!')
-    assert sum_module(3, 6) == 1
-    print('Sum of 10 and 10: ', sum_module(10, 10))
+    main()
+    # print('Hi there, I am module!')
+    # assert sum_module(3, 6) == 1
+    # print('Sum of 10 and 10: ', sum_module(10, 10))
