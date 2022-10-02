e2b439595cb2dd52b885ef190192eaa900ae4169
boyko1989 <hunwiss89@gmail.com>
2022-10-02 19:18:59 +0400

    Parameter dest
    В Namespace через параметр попадает ключ = значению этого
    параметра и значением, которое мы передали.

--- 04_add_argument_method.py
+++ 04_add_argument_method.py
@@ -2,22 +2,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("a", help="First argument", type=int)
-parser.add_argument("b", help="Second argument", type=int)
-
-parser.add_argument("-m", choices=['1', '2', '3'])
+parser.add_argument("-m", dest='B')
 
 args = parser.parse_args()
 
 print(args)
-
-def summarize (a, b):
-    print(a + b)
-
-def minusize (a, b):
-    print(a - b)
-
-if args.m == False:
-    summarize(args.a, args.b)
-elif args.m == True:
-    minusize(args.a, args.b)
