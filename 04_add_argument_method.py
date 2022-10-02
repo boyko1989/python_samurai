f0e951cc60cec40d044d9f792687aa64a0790804
boyko1989 <hunwiss89@gmail.com>
2022-10-02 18:42:48 +0400

    Practical Implementation of Flags
    В этом сучае мы передаём флаг -m, если нужно вычесть первый
    аргумент из второго. Сложение происходит по-умолчанию.
    А во всех остальных случаях мы "ругаемся"

--- 04_add_argument_method.py
+++ 04_add_argument_method.py
@@ -2,8 +2,24 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-a", "--action", action="store_const", const="value")
+parser.add_argument("a", help="First argument", type=int)
+parser.add_argument("b", help="Second argument", type=int)
+
+parser.add_argument("-m", action="store_const", const="minusize")
 
 args = parser.parse_args()
 
 print(args)
+
+def summarize (a, b):
+    print(a + b)
+
+def minusize (a, b):
+    print(a - b)
+
+if args.m == None:
+    summarize(args.a, args.b)
+elif args.m == 'minusize':
+    minusize(args.a, args.b)
+else:
+    print('You must choose action')
