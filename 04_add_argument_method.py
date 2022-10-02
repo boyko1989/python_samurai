f42ac3d5e3ece1f34d9d80ddd273a4753f7ccc84
boyko1989 <hunwiss89@gmail.com>
2022-10-02 18:31:02 +0400

    Combination of value store_const and const=""
    Это сочетание превращает создаваемый параметр во флаг.
    Таким образом передавая данный параметр мы можем не прописывать его
    значение.

--- 04_add_argument_method.py
+++ 04_add_argument_method.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-a", "--action", action="store")
+parser.add_argument("-a", "--action", action="store_const", const="value")
 
 args = parser.parse_args()
 
